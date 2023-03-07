959cb629631f9deab26b696ff89993acab774759
DongHee980630 <ehdehdrnt5@gmail.com>
2023-03-07 23:43:31 +0900

    2

--- src/main/java/com/sparta/springjwt/hanghaepost/controller/PostController.java
+++ src/main/java/com/sparta/springjwt/hanghaepost/controller/PostController.java
@@ -1,2 +1,36 @@
-package com.sparta.springjwt.hanghaepost.controller;public class PostController {
+package com.sparta.springjwt.hanghaepost.controller;
+
+import com.sparta.springjwt.hanghaepost.dto.PostRequestsDto;
+import com.sparta.springjwt.hanghaepost.dto.PostResponseDto;
+import com.sparta.springjwt.hanghaepost.entity.Post;
+import com.sparta.springjwt.hanghaepost.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping("/api/posts") public PostResponseDto createPost(@RequestBody PostRequestsDto requestsDto, HttpServletRequest request){
+        return postService.createPost(requestsDto, request);
+    }
+    @GetMapping("/api/posts")
+    public List<Post> getPosts(){
+        return postService.getPosts();
+    }
+
+    @PutMapping("/api/posts/{id}")
+    public Long updatePost(@PathVariable Long id, @RequestBody PostRequestsDto requestDto, HttpServletRequest request){
+        return postService.update(id, requestDto, request);
+    }
+
+    @DeleteMapping("/api/posts/{id}")
+    public Long deletePost(@PathVariable Long id, @RequestBody PostRequestsDto requestDto, HttpServletRequest request){
+        return postService.deletePost(id, requestDto, request);
+    }
 }
--- src/main/java/com/sparta/springjwt/hanghaepost/dto/PostResponseDto.java
+++ src/main/java/com/sparta/springjwt/hanghaepost/dto/PostResponseDto.java
@@ -1,2 +1,20 @@
-package com.sparta.springjwt.hanghaepost.dto;public class PostResponseDto {
+package com.sparta.springjwt.hanghaepost.dto;
+
+import com.sparta.springjwt.hanghaepost.entity.Post;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostResponseDto {
+    private Long id;
+    private String title;
+    private String contents;
+
+
+    public PostResponseDto(Post post) {
+        this.id = post.getId();
+        this.title = post.getTitle();
+        this.contents = post.getContents();
+    }
 }
